6f8cf3f85a7c3e708e92092b4528acef8dcac7b1
kiran <kirantsct@gmail.com>
2024-07-07 13:22:43 +0530

    910236

--- Page-Object-Model-POM-Design-With-Selenium-master/src/main/java/com/crm/qa/pages/DealsPage.java
+++ Page-Object-Model-POM-Design-With-Selenium-master/src/main/java/com/crm/qa/pages/DealsPage.java
@@ -3,6 +3,6 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class DealsPage extends TestBase{
-    public void(){}
+
 
 }
--- Page-Object-Model-POM-Design-With-Selenium-master/src/main/java/com/crm/qa/util/TestUtil.java
+++ Page-Object-Model-POM-Design-With-Selenium-master/src/main/java/com/crm/qa/util/TestUtil.java
@@ -1,22 +1,33 @@
 package com.crm.qa.util;
 
 import com.crm.qa.base.TestBase;
+import com.gargoylesoftware.htmlunit.javascript.host.file.File;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
-public class TestUtil extends TestBase{
-	
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestUtil extends TestBase {
+
 	public static long PAGE_LOAD_TIMEOUT = 60;
 	public static long IMPLICIT_WAIT = 20;
-	
+
 	public static String TESTDATA_SHEET_PATH = "C:\\Users\\Home\\workspace\\FreeCRMTest\\src\\main\\java\\"
 			+ "com\\crm\\qa\\testdata\\FreeCrmTestData.xlsx";
-	
+
 	static Workbook book;
 	static Sheet sheet;
-	
-	public void switchToFrame(){
+
+	public void switchToFrame() {
 		driver.switchTo().frame("mainpanel");
 	}
-	
+
 	public static Object[][] getTestData(String sheetName) { // very imp method for data driven framework
 		FileInputStream file = null;
 		try {
@@ -31,26 +42,29 @@ public class TestUtil extends TestBase{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		sheet = book.getSheet(sheetName);
-		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()]; // return type of data is 2 dimensional object array, eg: 2nd row and 1st column
-		// System.out.println(sheet.getLastRowNum() + "--------" +
-		// sheet.getRow(0).getLastCellNum());
-		for (int i = 0; i < sheet.getLastRowNum(); i++) { // 2 for loops for row and column
-			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
-				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
-				// System.out.println(data[i][k]);
-			}
-		}
-		return data;
+		return new Object[0][];
 	}
-	
-	public static void takeScreenshotAtEndOfTest() throws IOException {
-		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		String currentDir = System.getProperty("user.dir");		
-		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png")); // + means append with current timemillis
-		
-		}
-	
-	
 }
+
+//		sheet = (Sheet) book.getSheet(sheetName);
+//		Object[][] data = new Object[sheet.getBackground()][sheet.getRow(0).getLastCellNum()]; // return type of data is 2 dimensional object array, eg: 2nd row and 1st column
+//		// System.out.println(sheet.getLastRowNum() + "--------" +
+//		// sheet.getRow(0).getLastCellNum());
+//		for (int i = 0; i < sheet.getBackground(); i++) { // 2 for loops for row and column
+//			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+//				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+//				// System.out.println(data[i][k]);
+//			}
+//		}
+//		return data;
+//	}
+//
+//	public static void takeScreenshotAtEndOfTest() throws IOException {
+//		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//		String currentDir = System.getProperty("user.dir");
+//		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png")); // + means append with current timemillis
+//
+//
+
+
 	
